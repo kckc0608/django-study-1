3e0ee943e8b5e8c834449a01f8be152429f1bc9b
kckc0608 <kckc0608@gmail.com>
2023-11-05 00:09:17 +0900

    3-19. 에러 페이지 추가

--- foods/views.py
+++ foods/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -14,5 +15,9 @@ def food_detail(request, food):
         context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
         context["price"] = 10000
         context["img_path"] = "foods/images/chicken.jpg"
+    
+    else:
+        raise Http404("이런 음식은 없닥구요!")
+        
         
     return render(request, 'foods/detail.html', context=context)
